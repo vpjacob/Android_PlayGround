61e680af2487f98b50d0b74865b25f855760a227
vpjacob <824629419@qq.com>
2017-08-17 13:18:46 +0800

    增加版本显示

---
+++ VersionCode/app/src/main/java/com/example/liuyi/versioncode/VersionCode.java
@@ -0,0 +1,35 @@
+package com.example.liuyi.versioncode;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class VersionCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_version_code);
+
+        String string = getVersion();
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText("版本号：" + string);
+    }
+
+    public String getVersion() {
+        try {
+            PackageManager manager = this.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(this.getPackageName(), 0);
+            String version = info.versionName;
+            return version;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "error";
+        }
+    }
+
+}
+
+
