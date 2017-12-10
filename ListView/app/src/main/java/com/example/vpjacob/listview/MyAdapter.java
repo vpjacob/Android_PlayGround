e94f3d15ad42bdf386e05feb4ffc32811df815f8
vpjacob <824629419@qq.com>
2017-12-10 20:18:15 +0800

    继续as

--- ListView/app/src/main/java/com/example/vpjacob/listview/MyAdapter.java
+++ ListView/app/src/main/java/com/example/vpjacob/listview/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.example.vpjacob.listview;
 
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
@@ -37,16 +38,59 @@ public class MyAdapter extends BaseAdapter {
         return 0;
     }
 
+
+    class myHolder{
+        TextView textView1;
+        TextView textView2;
+    }
+
+//    @Override
+//    public View getView(int i, View view, ViewGroup viewGroup) {
+//
+////        Log.d(TAG, "getView: " + i);
+////        TextView textView = new TextView(viewGroup.getContext());
+////        textView.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT,AbsListView.LayoutParams.WRAP_CONTENT));
+////        textView.setTextSize(30);
+////        textView.setText(mDatas.get(i));
+//
+//        //布局充气
+//        LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
+//        View view1 = layoutInflater.inflate(R.layout.lv_item_layout,null);
+//        TextView textView = (TextView) view1.findViewById(R.id.lv_item);
+//        textView.setText(mDatas.get(i));
+//        return textView;
+//
+//
+//    }
+
+
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+        myHolder myHolder = null;
 
-        Log.d(TAG, "getView: " + i);
-        TextView textView = new TextView(viewGroup.getContext());
-        textView.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT,AbsListView.LayoutParams.WRAP_CONTENT));
-        textView.setTextSize(30);
-        textView.setText(mDatas.get(i));
-        return textView;
+        if (view == null){
+            //布局充气
+
+            LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
+            view = layoutInflater.inflate(android.R.layout.simple_list_item_2,null);
+
+            myHolder = new myHolder();
+            myHolder.textView1 = (TextView) view.findViewById(android.R.id.text1);
+            myHolder.textView2 = (TextView) view.findViewById(android.R.id.text2);
+
+            view.setTag(myHolder);
+        }else {
+            myHolder =(myHolder) view.getTag();
+        }
+
+        myHolder.textView1.setText(mDatas.get(i));
+        myHolder.textView2.setText(mDatas.get(i));
+
+            return view;
+
+
     }
+
 
 
 }
